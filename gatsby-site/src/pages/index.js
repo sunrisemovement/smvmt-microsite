857351f021c70acb4eb5779328701476547c2b4c
Farwa Naqi <farwa.naqi@gmail.com>
2020-04-12 12:32:56 -0700

    Update index.js

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,11 +1,10 @@
 import React from "react"
-import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import SEO from "../components/seo"
 
 const IndexPage = () => (
   <Layout>
+    <h1>Hi people</h1>
   </Layout>
 )
 
