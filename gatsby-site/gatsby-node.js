bf8be927625b85e341246f55f15c1ecdd16f1348
Luke Westby <lukewestby1@gmail.com>
2020-05-08 11:35:04 -0700

    Add jsdoc types for API request
    Add some jsdoc type info about the data we're working with so VSCode will help us out. If we want to switch to typescript at some point we can turn these into type definitions.

--- gatsby-site/gatsby-node.js
+++ gatsby-site/gatsby-node.js
@@ -1,7 +1,51 @@
 /**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
+ * @typedef {Object} RemoteFileThumbnail
+ * @property {number} height
+ * @property {number} width
+ * @property {string} url
  */
 
-// You can delete this file if you're not using it
+/**
+ * @typedef {Object} RemoteFile
+ * @property {string} id
+ * @property {string} filename
+ * @property {number} size
+ * @property {string} type
+ * @property {string} url
+ * @property {Object} thumbnails
+ * @property {RemoteFileThumbnail | undefined} thumbnails.small
+ * @property {RemoteFileThumbnail | undefined} thumbnails.large
+ * @property {RemoteFileThumbnail | undefined} thumbnails.full
+ */
+
+/**
+ * @typedef {Object} HubLeader
+ * @property {string} first_name
+ * @property {string} last_name
+ * @property {string} email
+ */
+
+/**
+ * @typedef {Object} Hub
+ * @property {string} name
+ * @property {string} about
+ * @property {string} city
+ * @property {string} state
+ * @property {string} email
+ * @property {string | null} custom_weblink_text
+ * @property {string | null} website
+ * @property {string | null} facebook
+ * @property {string | null} instagram
+ * @property {string | null} twitter
+ * @property {string | null} signup_link
+ * @property {Array<HubLeader>} leaders
+ * @property {Array<RemoteFile> | null} documents
+ * @property {Array<RemoteFile> | null} images
+ */
+
+/**
+ * @typedef {Object} HubhubPayload
+ * @property {string} updated_at
+ * @property {Array<Hub>} map_data
+ */
+
