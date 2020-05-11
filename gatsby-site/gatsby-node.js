28befac5535b2208ce71549752bd203f28af8a1d
Luke Westby <lukewestby1@gmail.com>
2020-05-10 20:27:28 -0700

    Initial hubhub integration (#6)
    * Add jsdoc types for API request
    
    Add some jsdoc type info about the data we're working with so VSCode will help us out. If we want to switch to typescript at some point we can turn these into type definitions.
    
    * sourceNodes scaffold
    
    We're going to be using the sourceNodes hook to download data and put it into the site schema.
    
    * Download hub data
    
    To fill out our schema we'll fetch the hub data from the hubhub endpoint. We need to search for a specific hub name, which we will configure elsewhere. For now we'll just stick with something fake from the dev dataset that is deployed at this endpoint.
    
    * Create a node with toplevel data
    
    Pull off some fields that look relevant to this project and create a gatsby node. At this point you should be able to see the hub with [this localhost query](http://localhost:8000/__graphql?query=query%20MyQuery%20%7B%0A%20%20hub%20%7B%0A%20%20%20%20name%0A%20%20%7D%0A%7D%0A).
    
    * Link hub to files
    
    This is the trickiest part. Each hub in hubhub has a set of images and a set of documents that are references to some file in airtable. The gatsby-source-filesystem plugin creates its own quasi-standard definition for a `File` type in a gatsby schema, so we'll convert those links to the standard format using `createRemoteFileNode`. After creating the files we need to associate them with the correct field on the `Hub`, which is done in gatsby by appending `___NODE` to the end of the field name.
    
    * Add schema definition for a Hub
    
    In order to make things in the schema match the expectations of our API exactly, we can make a custom type definition. Notably, certain fields are marked as non-null, and the file links are always available as well so they are marked as non-null arrays of non-null. Lastly, when making a custom type gatsby's defaults get overridden so you have to specify your linked nodes with `@link`.
    
    * fix typo

--- gatsby-site/gatsby-node.js
+++ gatsby-site/gatsby-node.js
@@ -1,7 +1,168 @@
+const fetch = require("node-fetch").default
+const { createRemoteFileNode } = require("gatsby-source-filesystem")
+
 /**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
+ * @typedef {Object} RemoteFileThumbnail
+ * @property {number} height
+ * @property {number} width
+ * @property {string} url
  */
 
-// You can delete this file if you're not using it
+/**
+ * @typedef {Object} RemoteFile
+ * @property {string} id
+ * @property {string} filename
+ * @property {number} size
+ * @property {string} type
+ * @property {string} url
+ * @property {Object} thumbnails
+ * @property {RemoteFileThumbnail | undefined} thumbnails.small
+ * @property {RemoteFileThumbnail | undefined} thumbnails.large
+ * @property {RemoteFileThumbnail | undefined} thumbnails.full
+ */
+
+/**
+ * @typedef {Object} HubLeader
+ * @property {string} first_name
+ * @property {string} last_name
+ * @property {string} email
+ */
+
+/**
+ * @typedef {Object} Hub
+ * @property {string} name
+ * @property {string} about
+ * @property {string} city
+ * @property {string} state
+ * @property {string} email
+ * @property {string | null} custom_weblink_text
+ * @property {string | null} website
+ * @property {string | null} facebook
+ * @property {string | null} instagram
+ * @property {string | null} twitter
+ * @property {string | null} signup_link
+ * @property {Array<HubLeader>} leaders
+ * @property {Array<RemoteFile> | null} documents
+ * @property {Array<RemoteFile> | null} images
+ */
+
+/**
+ * @typedef {Object} HubhubPayload
+ * @property {string} updated_at
+ * @property {Array<Hub>} map_data
+ */
+
+const ENDPOINT = "https://sunrise-hub-json-staging.s3.amazonaws.com/hubs.json"
+const HUB_NAME = "Sunrise Foof"
+
+/**
+ * @param {import("gatsby").SourceNodesArgs} pluginArgs
+ */
+exports.sourceNodes = async ({
+  actions,
+  createContentDigest,
+  createNodeId,
+  reporter,
+  cache,
+  store,
+}) => {
+  const { createNode } = actions
+
+  const response = await fetch(ENDPOINT).catch(error => {
+    reporter.panicOnBuild(
+      `Request to hubhub endpoint ${ENDPOINT} could not send.`,
+      error
+    )
+    throw error
+  })
+
+  if (!response.ok) {
+    reporter.panicOnBuild(
+      `Request to hubhub endpoint ${ENDPOINT} failed with status code ${response.status}.`
+    )
+    return
+  }
+
+  /** @type {HubhubPayload} */
+  const data = await response.json()
+
+  const hub = data.map_data.find(hub => hub.name === HUB_NAME)
+
+  if (!hub) {
+    reporter.panicOnBuild(
+      `Hub "${HUB_NAME}" does not exist in Hubhub. Please check your config, or else contact a Hubhub admin.`
+    )
+  }
+
+  const hubNodeData = {
+    name: hub.name,
+    about: hub.about,
+    email: hub.email,
+    website: hub.website,
+    facebook: hub.facebook,
+    instagram: hub.instagram,
+    twitter: hub.twitter,
+  }
+
+  const hubNodeLinks = {
+    documents___NODE: !hub.documents
+      ? []
+      : await Promise.all(
+          hub.documents.map(async remoteFile => {
+            const fileNode = await createRemoteFileNode({
+              url: remoteFile.url,
+              name: remoteFile.filename,
+              cache: cache,
+              createNode: createNode,
+              createNodeId: createNodeId,
+              reporter: reporter,
+              store: store,
+            })
+            return fileNode.id
+          })
+        ),
+    images___NODE: await Promise.all(
+      hub.images.map(async remoteFile => {
+        const fileNode = await createRemoteFileNode({
+          url: remoteFile.url,
+          name: remoteFile.filename,
+          cache: cache,
+          createNode: createNode,
+          createNodeId: createNodeId,
+          reporter: reporter,
+          store: store,
+        })
+        return fileNode.id
+      })
+    ),
+  }
+
+  createNode({
+    id: createNodeId(`Hub-${hub.name}`),
+    ...hubNodeData,
+    ...hubNodeLinks,
+    internal: {
+      type: "Hub",
+      contentDigest: createContentDigest(hubNodeData),
+    },
+  })
+}
+
+exports.createSchemaCustomization = ({ actions }) => {
+  const { createTypes } = actions
+  const typeDefs = `
+    type Hub implements Node {
+      id: ID!
+      name: String!
+      about: String!
+      email: String!
+      website: String
+      facebook: String
+      instagram: String
+      twitter: String
+      documents: [File!]! @link(from: "documents___NODE")
+      images: [File!]! @link(from: "images___NODE")
+    }
+  `
+  createTypes(typeDefs)
+}
