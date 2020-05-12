33700d316591ed4bfcabb125350cecb69dc815dd
Luke Westby <lukewestby1@gmail.com>
2020-05-12 12:17:13 -0700

    Appease the typechecker
    This commit is just initial tidying. It's tough, because the JSDoc syntax can really be cumbersome at times but it's such a nice thing to have in place when you're dealing with data that comes in from a graphql query in gatsby and trying to map it onto your components.

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -1,7 +1,10 @@
 import React from "react"
 import Layout from "../components/layout"
 import Hero from "../components/Hero"
+
+// @ts-ignore
 import photoPlaceholderPath from "../images/photo-placeholder.svg"
+// @ts-ignore
 import sunriseLogoPath from "../images/sunrise-logo.png"
 
 const IndexPage = () => (
