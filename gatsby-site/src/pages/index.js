fd8156162c186b2e2047f1cc414dff39722fd234
Luke Westby <lukewestby1@gmail.com>
2020-05-18 21:29:03 -0700

    Pass data in to hero from index

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -16,21 +16,6 @@ const MainContent = styled.div`
   margin: 0 auto;
 `
 
-const IndexPage = () => (
-  <Layout>
-    <Hero
-      dense
-      background={photoPlaceholderPath}
-      hubLogo={sunriseLogoPath}
-      hubName="PDX"
-      hubWebsite="https://www.sunrisepdx.org"
-    />
-    <MainContent>
-      <About
-        hubName="PDX"
-        content={
-          "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-        }
 /**
  * @typedef {Object} IndexPageProps
  * @property {PageQuery} data
@@ -38,10 +23,26 @@ const IndexPage = () => (
  * @param {IndexPageProps} props
  */
 const IndexPage = ({ data }) => {
+  return (
+    <Layout>
+      <Hero
+        dense
+        background={data.hub.hero.childImageSharp}
+        hubLogo={data.hub.logo.childImageSharp}
+        hubName={data.hub.name}
+        hubWebsite={data.hub.website}
       />
-    </MainContent>
-  </Layout>
-)
+      <MainContent>
+        <About
+          hubName="PDX"
+          content={
+            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+          }
+        />
+      </MainContent>
+    </Layout>
+  )
+}
 
 export default IndexPage
 
