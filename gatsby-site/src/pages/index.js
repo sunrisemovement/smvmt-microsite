fc16bce16cbd706bb1121b92fb1179bc5e7f4165
Luke Westby <lukewestby1@gmail.com>
2020-05-18 21:20:20 -0700

    Write graphql query for Hero

--- gatsby-site/src/pages/index.js
+++ gatsby-site/src/pages/index.js
@@ -8,6 +8,7 @@ import About from "../components/About"
 import photoPlaceholderPath from "../images/photo-placeholder.svg"
 // @ts-ignore
 import sunriseLogoPath from "../images/sunrise-logo.png"
+import { graphql } from "gatsby"
 
 const MainContent = styled.div`
   max-width: 756px;
@@ -30,9 +31,50 @@ const IndexPage = () => (
         content={
           "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.\n\n\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
         }
+/**
+ * @typedef {Object} IndexPageProps
+ * @property {PageQuery} data
+ *
+ * @param {IndexPageProps} props
+ */
+const IndexPage = ({ data }) => {
       />
     </MainContent>
   </Layout>
 )
 
 export default IndexPage
+
+/**
+ * @typedef {Object} PageQuery
+ * @property {Object} hub
+ * @property {string} hub.name
+ * @property {string | null} hub.website
+ * @property {Object} hub.logo
+ * @property {import("gatsby-image").GatsbyImageProps} hub.logo.childImageSharp
+ * @property {Object} hub.hero
+ * @property {import("gatsby-image").GatsbyImageProps} hub.hero.childImageSharp
+ */
+
+export const pageQuery = graphql`
+  query IndexPage {
+    hub {
+      name
+      website
+      logo {
+        childImageSharp {
+          fluid(maxWidth: 28) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+      hero {
+        childImageSharp {
+          fluid(maxWidth: 1440) {
+            ...GatsbyImageSharpFluid
+          }
+        }
+      }
+    }
+  }
+`
