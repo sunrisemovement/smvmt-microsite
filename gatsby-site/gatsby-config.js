1e088e8486f0403e3d65d4c4ba99f17a8c96e69b
Luke Westby <lukewestby1@gmail.com>
2020-06-14 13:15:41 -0700

    cleanup, set up proper icons

--- gatsby-site/gatsby-config.js
+++ gatsby-site/gatsby-config.js
@@ -18,13 +18,13 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Sunrise Movement Hubs`,
+        short_name: `Sunrise Hubs`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#33342E`,
+        theme_color: `#FFDE16`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        icon: `src/images/sunrise-logo.png`, // This path is relative to the root of the site.
       },
     },
     {
