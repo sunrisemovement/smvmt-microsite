877550580e4ee3571ef888896059d32f32a64020
Luke Westby <lukewestby1@gmail.com>
2020-07-29 10:42:05 -0700

    Rewrite uppercase routes
    GitHub Pages can't do case-insensitive routing, and all of our slugs are all lowercase so we have to do it on the client. When we hit a 404, check if the pathname can be rewritten to all lowercase and go there before showing a real 404 page.

--- gatsby-site/src/pages/404.js
+++ gatsby-site/src/pages/404.js
@@ -1,14 +1,25 @@
 import React from "react"
+import { useLocation, useNavigate } from "@reach/router"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
-)
+const NotFoundPage = () => {
+  const location = useLocation()
+  const navigate = useNavigate()
+
+  React.useLayoutEffect(() => {
+    if (location.pathname.toLowerCase() === location.pathname) return
+    navigate(location.pathname.toLowerCase())
+  }, [location.pathname, navigate])
+
+  return (
+    <div>
+      <SEO title="404: Not found" />
+      <h1>NOT FOUND</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+  )
+}
 
 export default NotFoundPage
